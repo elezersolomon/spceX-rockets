22162c168dc12a3906e3d1013843ed724cd09619
elezersolomon <elezersolomon2@gmail.com>
2022-11-16 01:30:57 +0300

    preparing for deployment to render

---
+++ public/build/precache-manifest.3ad0f462a5a0c46e0c3d5d32f3c88a19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817445239c6b5d0c2304a24591979840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86bb2daf5dedebfd0114",
+    "url": "/static/css/main.2062af5d.chunk.css"
+  },
+  {
+    "revision": "a76c600430073018dd49",
+    "url": "/static/js/2.779b9e7f.chunk.js"
+  },
+  {
+    "revision": "5619cfe8e7dcc0737172d69545589a05",
+    "url": "/static/js/2.779b9e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86bb2daf5dedebfd0114",
+    "url": "/static/js/main.79f3c876.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "88c8085379c2f611c56b38d342186e93",
+    "url": "/static/media/SpaceX Logo.88c80853.jpg"
+  }
+]);
